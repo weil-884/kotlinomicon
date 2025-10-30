3f536089e92cbed9b588a1683c350e05822af59d
Bartosz P. <bartosz2006.bp@gmail.com>
2025-10-30 15:41:43 +0100

    it should work now

---
+++ cherries/src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+package org.example
+
+fun main() {
+    val name = "Kotlin"
+    println("Hello, $name!")
+
+    println("damn")
+
+    for (i in 1..5) {
+                println("i = $i")
+    }
+}
